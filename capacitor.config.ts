5827b561bae72dcbf9fc68c108b44e61cfa85dc9
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2022-08-31 14:36:54 -0300

    first commit: adding capacitor and camera

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.expirationcontrol.app',
+  appName: 'Expiration Date Control',
+  webDir: 'dist/expiration-date-control',
+  bundledWebRuntime: false
+};
+
+export default config;
